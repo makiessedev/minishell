38ff8a4d574f3d1cf1a8008c4e103efe8a09b9f3
Makiesse <makiesse.dev@gmail.com>
2025-01-27 23:26:59 +0100

    feat: initial setup

--- include/minishell.h
+++ include/minishell.h
@@ -3,9 +3,24 @@
 
 #include "libft.h"
 #include <stdio.h>
-#include <stdlib.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+
+#ifndef _STDBOOL_H_
+#define _STDBOOL_H_
+# define bool int
+# define true 1
+# define false 0
+#endif
+
+# ifndef PATH_SIZE
+#  define PATH_SIZE (1024*4)
+# endif
+
+typedef struct s_minishell
+{
+    char	**envp;
+}	t_minishell;
 
 void	launch_executable(char *full_cmds);
 
--- src/main.c
+++ src/main.c
@@ -1,11 +1,80 @@
 #include "minishell.h"
 
-int		main(int ac, char **av)
+static int get_var_index(char *var, char **envp)
 {
-	char	*line;
+	int	i;
 
-	(void)ac;
-	(void)av;
+	i = 0;
+	while (envp[i])
+	{
+		if (ft_strncmp(envp[i], var, ft_strlen(var)) == 0)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+static bool is_existing_var(char *var, char **envp)
+{
+	if (get_var_index(var, envp) == -1)
+		return (false);
+	return (true);
+}
+
+static char *get_var_value(char *var, char **envp)
+{
+	int	index;
+	char	*value;
+
+	index = get_var_index(var, envp);
+	if (index == -1)
+		return (NULL);
+	value = ft_strchr(envp[index], '=') + 1;
+	return (value);
+}
+
+static bool setup_workdir(t_minishell *minishell_data)
+{
+	char buffer[PATH_SIZE];
+	cwd = getcwd(buffer, PATH_SIZE);
+	if (cwd == NULL)
+		return (ft_putstr_fd("minishell: getcwd failed\n"), false);
+	minishell_data->current_workdir = ft_strdup(cwd);
+	if (is_existing_var("OLDPWD", minishell_data->envp))
+	{
+		minishell_data->old_workdir = get_var_value("OLDPWD", minishell_data->envp);
+		if (!minishell_data->old_workdir)
+			return (ft_putstr_fd("minishell: get_var_value failed\n"), false);
+	}
+	else
+	{
+		minishell_data->old_workdir = ft_strdup(minishell_data->current_workdir);
+		if (!minishell_data->old_workdir)
+			return (ft_putstr_fd("minishell: ft_strdup failed\n"), false);
+	}
+	return (true);
+}
+
+static bool init_setup(t_minishell *minishell_data, char **envp)
+{
+	minishell_data->envp = envp;
+	if (!setup_workdir(minishell_data->current_workdir))
+		return (false);
+	return (true);
+}
+
+int		main(int ac, char **av, char **envp)
+{
+	char		*line;
+	t_minishell	*minisheel_data;
+
+	if (ac != 1)
+	{
+		ft_putstr_fd("minishell: No arguments are allowed\n");
+		return (1);
+	}
+	init_setup(minisheel_data, envp);
+
 	while (1)
 	{
 		line = readline("minishell$ ");
