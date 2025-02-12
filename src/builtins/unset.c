d40c34ba51e9dc28649d7fad13d627cd89ca21e2
Makiesse Morais <makiesse.dev@gmail.com>
2025-02-12 15:18:34 +0100

    feat: unset builtin

---
+++ src/builtins/unset.c
@@ -0,0 +1,27 @@
+#include "minishell.h"
+
+int	unset_builtin(t_data *data, char **args)
+{
+	int	i;
+	int	idx;
+	int	ret;
+
+	ret = EXIT_SUCCESS;
+	i = 1;
+	while (args[i])
+	{
+		if (!is_valid_env_var_key(args[i]) || ft_strchr(args[i], '=') != NULL)
+		{
+			errmsg_cmd("unset", args[i], "not a valid identifier", false);
+			ret = EXIT_FAILURE;
+		}
+		else
+		{
+			idx = get_env_var_index(data->env, args[i]);
+			if (idx != -1)
+				remove_env_var(data, idx);
+		}
+		i++;
+	}
+	return (ret);
+}
