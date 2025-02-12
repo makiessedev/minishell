45276744ebaee8c05b7424b9f20afa63a83bcf89
Makiesse Morais <makiesse.dev@gmail.com>
2025-02-12 15:13:30 +0100

    feat: env builtin

---
+++ src/builtins/env.c
@@ -0,0 +1,15 @@
+#include "minishell.h"
+
+int	env_builtin(t_data *data, char **args)
+{
+	int	i;
+
+	if (args && args[1])
+		return (errmsg_cmd("env", NULL, "too many arguments", 2));
+	i = 0;
+	if (!data->env)
+		return (EXIT_FAILURE);
+	while (data->env[i])
+		ft_putendl_fd(data->env[i++], STDOUT_FILENO);
+	return (EXIT_SUCCESS);
+}
