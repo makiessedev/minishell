2736a09a3fbc47e4c6354fcffa3ece936908e613
Makiesse Morais <makiesse.dev@gmail.com>
2025-04-11 05:50:11 +0100

    fix error message on unset invalid identifier

--- src/commands/exec_pwd.c
+++ src/commands/exec_pwd.c
@@ -1,22 +1,19 @@
 #include "minishell.h"
 
-int	exec_pwd(t_main *main_data, char **args)
-{
-	char	buf[PATH_MAX];
-	char	*cwd;
+int exec_pwd(t_main *main_data, char **args) {
+  char buf[PATH_MAX];
+  char *cwd;
 
-	(void)args;
-	if (main_data->working_dir)
-	{
-		ft_putendl_fd(main_data->working_dir, STDOUT_FILENO);
-		return (EXIT_SUCCESS);
-	}
-	cwd = getcwd(buf, PATH_MAX);
-	if (cwd)
-	{
-		ft_putendl_fd(cwd, STDOUT_FILENO);
-		return (EXIT_SUCCESS);
-	}
-	throw_command_error("pwd", NULL, strerror(errno), errno);
-	return (EXIT_FAILURE);
+  (void)args;
+  if (main_data->working_dir) {
+    ft_putendl_fd(main_data->working_dir, STDOUT_FILENO);
+    return (EXIT_SUCCESS);
+  }
+  cwd = getcwd(buf, PATH_MAX);
+  if (cwd) {
+    ft_putendl_fd(cwd, STDOUT_FILENO);
+    return (EXIT_SUCCESS);
+  }
+  throw_command_error("pwd", NULL, "pwd error", EXIT_FAILURE);
+  return (EXIT_FAILURE);
 }
--- src/commands/exec_unset.c
+++ src/commands/exec_unset.c
@@ -1,27 +1,21 @@
 #include "minishell.h"
 
-int	exec_unset(t_main *main_data, char **args)
-{
-	int	i;
-	int	idx;
-	int	ret;
+int exec_unset(t_main *main_data, char **args) {
+  int i;
+  int idx;
+  int ret;
 
-	ret = EXIT_SUCCESS;
-	i = 1;
-	while (args[i])
-	{
-		if (!is_valid_env_var_key(args[i]) || ft_strchr(args[i], '=') != NULL)
-		{
-			throw_command_error("unset", args[i], "not a valid identifier", false);
-			ret = EXIT_FAILURE;
-		}
-		else
-		{
-			idx = get_env_var_index(main_data->env, args[i]);
-			if (idx != -1)
-				remove_env_var(main_data, idx);
-		}
-		i++;
-	}
-	return (ret);
+  ret = EXIT_SUCCESS;
+  i = 1;
+  while (args[i]) {
+    if (!is_valid_env_var_key(args[i]) || ft_strchr(args[i], '=') != NULL)
+      ret = EXIT_FAILURE;
+    else {
+      idx = get_env_var_index(main_data->env, args[i]);
+      if (idx != -1)
+        remove_env_var(main_data, idx);
+    }
+    i++;
+  }
+  return (ret);
 }
