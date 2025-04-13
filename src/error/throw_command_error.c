8f6435cf0bd1ce2dc8f39f8c217d7af33e9314d4
Makiesse Morais <makiesse.dev@gmail.com>
2025-04-13 11:04:22 +0100

    remove special treatmen on error messages

--- src/error/throw_command_error.c
+++ src/error/throw_command_error.c
@@ -1,37 +1,18 @@
 #include "minishell.h"
 
-static bool	add_detail_quotes(char *command)
-{
-	if (ft_strncmp(command, "export", 7) == 0
-		|| ft_strncmp(command, "unset", 6) == 0)
-		return (true);
-	return (false);
-}
+int throw_command_error(char *command, char *detail, char *error_message,
+                        int error_nb) {
+  char *msg;
 
-
-int	throw_command_error(char *command, char *detail, char *error_message, int error_nb)
-{
-	char	*msg;
-	bool	detail_quotes;
-
-	detail_quotes = add_detail_quotes(command);
-	msg = ft_strdup("minishell: ");
-	if (command != NULL)
-	{
-		msg = join_strs(msg, command);
-		msg = join_strs(msg, ": ");
-	}
-	if (detail != NULL)
-	{
-		if (detail_quotes)
-			msg = join_strs(msg, "`");
-		msg = join_strs(msg, detail);
-		if (detail_quotes)
-			msg = join_strs(msg, "'");
-		msg = join_strs(msg, ": ");
-	}
-	msg = join_strs(msg, error_message);
-	ft_putendl_fd(msg, STDERR_FILENO);
-	erase_pointer(msg);
-	return (error_nb);
+  msg = ft_strdup("minishell: ");
+  if (command != NULL) {
+    msg = join_strs(msg, command);
+    msg = join_strs(msg, ": ");
+  }
+  if (detail != NULL)
+    msg = join_strs(msg, ": ");
+  msg = join_strs(msg, error_message);
+  ft_putendl_fd(msg, STDERR_FILENO);
+  erase_pointer(msg);
+  return (error_nb);
 }
--- src/error/throw_message_error.c
+++ src/error/throw_message_error.c
@@ -1,18 +1,17 @@
 #include "minishell.h"
 
-void	throw_message_error(char *errmsg, char *detail, int quotes)
-{
-	char	*msg;
+void throw_message_error(char *errmsg, char *detail, int quotes) {
+  char *msg;
 
-	msg = ft_strdup("minishell: ");
-	msg = join_strs(msg, errmsg);
-	if (quotes)
-		msg = join_strs(msg, " `");
-	else
-		msg = join_strs(msg, ": ");
-	msg = join_strs(msg, detail);
-	if (quotes)
-		msg = join_strs(msg, "'");
-	ft_putendl_fd(msg, STDERR_FILENO);
-	erase_pointer(msg);
+  msg = ft_strdup("minishell: ");
+  msg = join_strs(msg, errmsg);
+  if (quotes)
+    msg = join_strs(msg, " `");
+  else
+    msg = join_strs(msg, ": ");
+  msg = join_strs(msg, detail);
+  if (quotes)
+    msg = join_strs(msg, "'");
+  ft_putendl_fd(msg, STDERR_FILENO);
+  erase_pointer(msg);
 }
