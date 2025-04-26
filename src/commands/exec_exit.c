f79f1492e907ab721312dce0e4ff56e8f527538d
Makiesse Morais <makiesse.dev@gmail.com>
2025-04-26 21:42:47 +0100

    simplify code

--- src/commands/exec_exit.c
+++ src/commands/exec_exit.c
@@ -6,26 +6,13 @@
 /*   By: zombunga <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 17:53:14 by mmorais           #+#    #+#             */
-/*   Updated: 2025/04/26 21:03:01 by mmorais          ###   ########.fr       */
+/*   Updated: 2025/04/26 21:30:56 by mmorais          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include <stdlib.h>
 
-/*static int handle_exit_code(char **args, int *error) {
-  int exit_code;
-
-  if (!args || !args[1])
-    return (g_exit_code);
-  exit_code = get_exit_code(args[1], error);
-  if (*error)
-    return (
-        throw_command_error("exit", args[1], "numeric argument required", 2));
-  if (args[2])
-    return (throw_command_error("exit", NULL, "too many arguments", 1));
-  return (exit_code);
-}*/
-/*
 static void update_shell_level(t_main *main_data) {
   int shell_level;
   char *new_lvl;
@@ -37,22 +24,6 @@ static void update_shell_level(t_main *main_data) {
   if (new_lvl)
     update_or_create_env_variable(main_data, "SHLVL", new_lvl);
   free(new_lvl);
-}
-*/
-static int *exit_status_pointer(void);
-
-int exec_exit_status_get(void) { return (*exit_status_pointer()); }
-
-void exec_exit_status_set(int status) {
-  if (status == EXIT_FAILURE)
-    status = EXIT_FAILURE;
-  *exit_status_pointer() = status;
-}
-
-static int *exit_status_pointer(void) {
-  static int last_exit_status = 0;
-
-  return (&last_exit_status);
 }
 
 static int num_is_numeric(char *str) {
@@ -74,13 +45,14 @@ static int num_is_numeric(char *str) {
   return (TRUE);
 }
 
-int builtin_exit(int argc, char **argv) {
+int builtin_exit(t_main *main_data, int argc, char **argv) {
   int exit_num;
+  int quiet;
 
-  /*	if (!subshell && isatty(STDERR_FILENO))
-                  ft_putstr_fd("exit\n", STDERR_FILENO);
-          */
-  exit_num = exec_exit_status_get();
+  quiet = is_quiet_mode(main_data);
+  if (!quiet)
+    ft_putendl_fd("exit", 2);
+  exit_num = g_exit_code;
   if (argc >= 2 && num_is_numeric(argv[1]) == FALSE)
     exit_num = 255;
   else if (argc == 2)
@@ -89,24 +61,31 @@ int builtin_exit(int argc, char **argv) {
     throw_command_error("exit", NULL, "too many arguments", 1);
     return (EXIT_FAILURE);
   }
-  exit(exit_num);
+  update_shell_level(main_data);
+  erase_and_exit_shell(main_data, exit_num);
+  return (EXIT_FAILURE);
 }
 
-int exec_exit(t_main *main_data, char **args) {
-  /*int exit_code;
-  int error;
-  int quiet;
+int exec_exit(t_main *main_data, char **argv) {
 
-  error = FALSE;
+  int exit_num;
+  int quiet;
+  int argc;
+
+  argc = count_tab(argv);
   quiet = is_quiet_mode(main_data);
   if (!quiet)
     ft_putendl_fd("exit", 2);
-  exit_code = handle_exit_code(args, &error);
-  if (error && args[2])
-    return (exit_code);
+  exit_num = g_exit_code;
+  if (argc >= 2 && num_is_numeric(argv[1]) == FALSE)
+    exit_num = 255;
+  else if (argc == 2)
+    exit_num = ft_atoi(argv[1]);
+  else if (argc > 2) {
+    throw_command_error("exit", NULL, "too many arguments", 1);
+    return (EXIT_FAILURE);
+  }
   update_shell_level(main_data);
-  erase_and_exit_shell(main_data, exit_code);*/
-  (void)main_data;
-  return (builtin_exit(count_tab(args), args));
-  // return (2);
+  erase_and_exit_shell(main_data, exit_num);
+  return (EXIT_FAILURE);
 }
